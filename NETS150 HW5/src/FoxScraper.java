d1c2a6a74f5c00db281744728a59fb0ddb86f6fe
Amy Guo <amyguo@seas.upenn.edu>
2020-05-06 19:06:48 -0400

    finished scrapers

--- NETS150 HW5/src/FoxScraper.java
+++ NETS150 HW5/src/FoxScraper.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,197 +12,208 @@ import org.jsoup.select.Elements;
 
 public class FoxScraper {
 
-	static List<String> allArticleLinks = new ArrayList<>();
-	static List<String> selectedArticleLinks = new ArrayList<>();
-	static List<String> articleContents = new ArrayList<>();
-	static List<String> articleHeadlines = new ArrayList<>();
+    static List<String> allArticleLinks = new ArrayList<>();
+    static Map<String, String> topTenFOXArticles = new HashMap<>();
+    static Map<String, String> worldNewsArticles = new HashMap<>();
+    static Map<String, String> usNewsArticles = new HashMap<>();
+    static Map<String, String> politicsArticles = new HashMap<>();
+    static Map<String, String> entertainmentArticles = new HashMap<>();
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		getArticles("https://www.foxnews.com", 10);
+        getArticles("https://www.foxnews.com", 10);
+        comparisons();
+    }
 
-		for (int i = 0; i < selectedArticleLinks.size(); i++) {
-			System.out.println(selectedArticleLinks.get(i));
-		}
-		for (int i = 0; i < selectedArticleLinks.size(); i++) {
-			getArticleText(selectedArticleLinks.get(i));
-		}
+    public static void getArticles(String url, int amount) throws IOException {
 
-		comparisons();
-		// System.out.println("Article contents size: " + articleContents.size());
+        Document document = Jsoup.connect(url).get();
+        Elements links = document.select("a[href]");
 
-//		for (int i = 0; i < selectedArticleLinks.size(); i++) {
-//			System.out.println(articleContents.get(i));
-//		}
-	}
+        for (Element link : links) {
+            allArticleLinks.add(link.attr("href"));
+        }
 
-	public static List<String> getArticles(String url, int amount) throws IOException {
+        int counter = 0;
+        int index = 0;
+        for (int i = 0; i < allArticleLinks.size(); i++) {
+            if (allArticleLinks.get(i).contains("mailto:")) {
+                index = i + 1;
+                break;
+            }
+        }
 
-		Document document = Jsoup.connect(url).get();
-		Elements links = document.select("a[href]");
+        while (counter != amount) {
+            if ((!allArticleLinks.get(index).contains("video")) && allArticleLinks.get(index).length() > 50) {
+                if (!topTenFOXArticles.containsKey(allArticleLinks.get(index))) {
+                    if (allArticleLinks.get(index).substring(0, 2).equals("//")) {
+                        topTenFOXArticles.put("https:" + allArticleLinks.get(index),
+                                getArticleText(allArticleLinks.get(index)));
+                    } else {
+                        topTenFOXArticles.put(allArticleLinks.get(index), getArticleText(allArticleLinks.get(index)));
+                    }
+                    counter++;
+                }
+            }
+            index++;
+            if (index >= allArticleLinks.size()) {
+                break;
+            }
+        }
 
-		for (Element link : links) {
-			allArticleLinks.add(link.attr("href"));
-		}
+        int target = 4;
+        counter = 0;
+        int index2 = index;
+        while (counter != target) {
+            if ((!allArticleLinks.get(index2).contains("video")) && allArticleLinks.get(index2).length() > 50) {
+                if (!topTenFOXArticles.containsKey(allArticleLinks.get(index2))) {
+                    if (allArticleLinks.get(index2).contains("/us/")) {
+                        if (allArticleLinks.get(index2).substring(0, 2).equals("//")) {
+                            usNewsArticles.put("https:" + allArticleLinks.get(index2),
+                                    getArticleText(allArticleLinks.get(index2)));
+                        } else {
+                            usNewsArticles.put(allArticleLinks.get(index2),
+                                    getArticleText(allArticleLinks.get(index2)));
+                        }
+                        counter++;
+                    }
+                }
+            }
+            index2++;
+            if (index2 >= allArticleLinks.size()) {
+                break;
+            }
+        }
 
-		int counter = 0;
-		int index = 0;
-		for (int i = 0; i < allArticleLinks.size(); i++) {
-			if (allArticleLinks.get(i).contains("mailto:")) {
-				index = i + 1;
-				break;
-			}
-		}
+        counter = 0;
+        index2 = index;
+        while (counter != target) {
+            if ((!allArticleLinks.get(index2).contains("video")) && allArticleLinks.get(index2).length() > 50) {
+                if (!topTenFOXArticles.containsKey(allArticleLinks.get(index2))) {
+                    if (allArticleLinks.get(index2).contains("/entertainment/")) {
+                        if (allArticleLinks.get(index2).substring(0, 2).equals("//")) {
+                            entertainmentArticles.put("https:" + allArticleLinks.get(index2),
+                                    getArticleText(allArticleLinks.get(index2)));
+                        } else {
+                            entertainmentArticles.put(allArticleLinks.get(index2),
+                                    getArticleText(allArticleLinks.get(index2)));
+                        }
+                        counter++;
+                    }
+                }
+            }
+            index2++;
+            if (index2 >= allArticleLinks.size()) {
+                break;
+            }
+        }
 
-		while (counter != amount) {
-			if ((!allArticleLinks.get(index).contains("video")) && allArticleLinks.get(index).length() > 50) {
-				if (!selectedArticleLinks.contains(allArticleLinks.get(index))) {
-					selectedArticleLinks.add(allArticleLinks.get(index));
-					// System.out.println(allArticleLinks.get(index));
-					counter++;
-				}
-			}
-			index++;
-		}
+        counter = 0;
+        index2 = index;
+        while (counter != target) {
+            if ((!allArticleLinks.get(index2).contains("video")) && allArticleLinks.get(index2).length() > 50) {
+                if (!topTenFOXArticles.containsKey(allArticleLinks.get(index2))) {
+                    if (allArticleLinks.get(index2).contains("/world/")) {
+                        if (allArticleLinks.get(index2).substring(0, 2).equals("//")) {
+                            worldNewsArticles.put("https:" + allArticleLinks.get(index2),
+                                    getArticleText(allArticleLinks.get(index2)));
+                        } else {
+                            worldNewsArticles.put(allArticleLinks.get(index2),
+                                    getArticleText(allArticleLinks.get(index2)));
+                        }
+                        counter++;
+                    }
+                }
+            }
+            index2++;
+            if (index2 >= allArticleLinks.size()) {
+                break;
+            }
+        }
 
-		counter = 0;
-		int index2 = index;
-		while (counter != 4) {
-			if ((!allArticleLinks.get(index2).contains("video")) && allArticleLinks.get(index2).length() > 50) {
-				if (!selectedArticleLinks.contains(allArticleLinks.get(index2))) {
-					if (allArticleLinks.get(index2).contains("/us/")) {
-						selectedArticleLinks.add(allArticleLinks.get(index2));
-						// System.out.println(allArticleLinks.get(index));
-						counter++;
-					}
-				}
-			}
-			index2++;
-		}
+        counter = 0;
+        index2 = index;
+        while (counter != target) {
+            if ((!allArticleLinks.get(index2).contains("video")) && allArticleLinks.get(index2).length() > 50) {
+                if (!topTenFOXArticles.containsKey(allArticleLinks.get(index2))) {
+                    if (allArticleLinks.get(index2).contains("/politics/")) {
+                        if (allArticleLinks.get(index2).substring(0, 2).equals("//")) {
+                            politicsArticles.put("https:" + allArticleLinks.get(index2),
+                                    getArticleText(allArticleLinks.get(index2)));
+                        } else {
+                            politicsArticles.put(allArticleLinks.get(index2),
+                                    getArticleText(allArticleLinks.get(index2)));
+                        }
+                        counter++;
+                    }
+                }
+            }
+            index2++;
+            if (index2 >= allArticleLinks.size()) {
+                break;
+            }
+        }
 
-		counter = 0;
-		index2 = index;
-		while (counter != 4) {
-			if ((!allArticleLinks.get(index2).contains("video")) && allArticleLinks.get(index2).length() > 50) {
-				if (!selectedArticleLinks.contains(allArticleLinks.get(index2))) {
-					if (allArticleLinks.get(index2).contains("/entertainment/")) {
-						selectedArticleLinks.add(allArticleLinks.get(index2));
-						// System.out.println(allArticleLinks.get(index));
-						counter++;
-					}
-				}
-			}
-			index2++;
-		}
+    }
 
-		counter = 0;
-		index2 = index;
-		while (counter != 4) {
-			if ((!allArticleLinks.get(index2).contains("video")) && allArticleLinks.get(index2).length() > 50) {
-				if (!selectedArticleLinks.contains(allArticleLinks.get(index2))) {
-					if (allArticleLinks.get(index2).contains("/world/")) {
-						selectedArticleLinks.add(allArticleLinks.get(index2));
-						// System.out.println(allArticleLinks.get(index));
-						counter++;
-					}
-				}
-			}
-			index2++;
-		}
+    public static String getArticleText(String url) throws IOException {
 
-		counter = 0;
-		index2 = index;
-		while (counter != 4) {
-			if ((!allArticleLinks.get(index2).contains("video")) && allArticleLinks.get(index2).length() > 50) {
-				if (!selectedArticleLinks.contains(allArticleLinks.get(index2))) {
-					if (allArticleLinks.get(index2).contains("/politics/")) {
-						selectedArticleLinks.add(allArticleLinks.get(index2));
-						// System.out.println(allArticleLinks.get(index));
-						counter++;
-					}
-				}
-			}
-			index2++;
-		}
+        Document doc = Jsoup.connect(url).get();
 
-		for (int i = 0; i < selectedArticleLinks.size(); i++) {
-			if (selectedArticleLinks.get(i).substring(0, 2).equals("//")) {
-				String placeholder = "https:" + selectedArticleLinks.get(i);
-				selectedArticleLinks.set(i, placeholder);
-				System.out.println(selectedArticleLinks.get(i));
-			}
-		}
+        Elements nameElements2 = doc.select("p");
 
-		return selectedArticleLinks;
-	}
+        String text = nameElements2.text().substring(161);
+        return text;
+    }
 
-	public static void getArticleText(String url) throws IOException {
+    public static void comparisons() {
 
-		Document doc = Jsoup.connect(url).get();
+        String articleText = "";
+        for (Entry<String, String> e : topTenFOXArticles.entrySet()) {
+            articleText += " " + e.getValue();
+        }
+        Article top10FOX = new Article(articleText, "Top 10 FOX Articles");
 
-		String title = doc.title();
+        articleText = "";
+        for (Entry<String, String> e : usNewsArticles.entrySet()) {
+            articleText += " " + e.getValue();
+        }
+        Article USFOX = new Article(articleText, "FOX US Articles");
 
-		Elements nameElements2 = doc.select("p");
+        articleText = "";
+        for (Entry<String, String> e : entertainmentArticles.entrySet()) {
+            articleText += " " + e.getValue();
+        }
+        Article entertainmentFOX = new Article(articleText, "FOX Entertainment Articles");
 
-		// System.out.printf("Title: %s%n", title);
+        articleText = "";
+        for (Entry<String, String> e : worldNewsArticles.entrySet()) {
+            articleText += " " + e.getValue();
+        }
+        Article worldFOX = new Article(articleText, "FOX World Articles");
 
-		String placeholder = nameElements2.text().substring(161);
-		articleContents.add(placeholder);
-		articleHeadlines.add(title);
+        articleText = "";
+        for (Entry<String, String> e : politicsArticles.entrySet()) {
+            articleText += " " + e.getValue();
+        }
+        Article politicsFOX = new Article(articleText, "FOX Politics Articles");
 
-	}
-
-	public static void comparisons() {
-
-		String placeholder = "";
-
-		for (int i = 0; i < 10; i++) {
-			placeholder = placeholder + articleContents.get(i);
-		}
-
-		Article top10FOX = new Article(placeholder, "Top 10 FOX Articles");
-
-		placeholder = "";
-
-		for (int i = 10; i < 14; i++) {
-			placeholder = placeholder + articleContents.get(i);
-		}
-		Article USFOX = new Article(placeholder, "FOX US Articles");
-
-		placeholder = "";
-		for (int i = 14; i < 18; i++) {
-			placeholder = placeholder + articleContents.get(i);
-		}
-		Article entertainmentFOX = new Article(placeholder, "FOX Entertainment Articles");
-
-		placeholder = "";
-		for (int i = 18; i < 22; i++) {
-			placeholder = placeholder + articleContents.get(i);
-		}
-		Article worldFOX = new Article(placeholder, "FOX World Articles");
-
-		placeholder = "";
-		for (int i = 22; i < 26; i++) {
-			placeholder = placeholder + articleContents.get(i);
-		}
-		Article politicsFOX = new Article(placeholder, "FOX Politics Articles");
-
-		ArrayList<Article> documents = new ArrayList<Article>();
-		documents.add(top10FOX);
-		documents.add(USFOX);
-		documents.add(entertainmentFOX);
-		documents.add(worldFOX);
-		documents.add(politicsFOX);
-		Corpus corpus = new Corpus(documents);
-		VectorSpaceModel vectorSpace = new VectorSpaceModel(corpus);
-		for (int i = 0; i < documents.size(); i++) {
-			for (int j = i + 1; j < documents.size(); j++) {
-				Article doc1 = documents.get(i);
-				Article doc2 = documents.get(j);
-				System.out.println("\nComparing " + doc1.getName() + " and " + doc2.getName());
-				System.out.println(vectorSpace.cosineSimilarity(doc1, doc2));
-			}
-		}
-	}
+        ArrayList<Article> documents = new ArrayList<Article>();
+        documents.add(top10FOX);
+        documents.add(USFOX);
+        documents.add(entertainmentFOX);
+        documents.add(worldFOX);
+        documents.add(politicsFOX);
+        Corpus corpus = new Corpus(documents);
+        VectorSpaceModel vectorSpace = new VectorSpaceModel(corpus);
+        for (int i = 0; i < documents.size(); i++) {
+            for (int j = i + 1; j < documents.size(); j++) {
+                Article doc1 = documents.get(i);
+                Article doc2 = documents.get(j);
+                System.out.println("\nComparing " + doc1.getName() + " and " + doc2.getName());
+                System.out.println(vectorSpace.cosineSimilarity(doc1, doc2));
+            }
+        }
+    }
 
 }
--- NETS150 HW5/src/NBCScraper.java
+++ NETS150 HW5/src/NBCScraper.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.Map.Entry;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,29 +9,27 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class NBCScraper {
-    
+
     static List<String> allArticleLinks = new ArrayList<>();
-    static List<String> selectedArticleLinks = new ArrayList<>();
-    static List<String> articleContents = new ArrayList<>();
-    static List<String> articleHeadlines = new ArrayList<>();
+    static Map<String, String> topTenNBCArticles = new HashMap<>();
+    static Map<String, String> worldNewsArticles = new HashMap<>();
+    static Map<String, String> usNewsArticles = new HashMap<>();
+    static Map<String, String> politicsArticles = new HashMap<>();
+    static Map<String, String> businessArticles = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
 
         getArticles("https://www.nbcnews.com", 10);
-        
-        for (int i = 0; i < selectedArticleLinks.size(); i++) {
-            System.out.println(selectedArticleLinks.get(i));
-        }
-       
+
+        comparisons();
+
     }
 
-    public static List<String> getArticles(String url, int amount) throws IOException {
+    public static void getArticles(String url, int amount) throws IOException {
 
         Document document = Jsoup.connect(url).get();
 
         Elements links = document.select("a[href]");
-        PrintWriter out = new PrintWriter("test.txt");
-        out.println(links);
 
         for (Element link : links) {
             allArticleLinks.add(link.attr("href"));
@@ -49,136 +47,135 @@ public class NBCScraper {
 
         while (counter != amount) {
             if ((!allArticleLinks.get(index).contains("_tps")) && allArticleLinks.get(index).length() > 60) {
-                if (!selectedArticleLinks.contains(allArticleLinks.get(index))) {
-                    selectedArticleLinks.add(allArticleLinks.get(index));
+                if (!topTenNBCArticles.containsKey(allArticleLinks.get(index))) {
+                    topTenNBCArticles.put(allArticleLinks.get(index), getArticleText(allArticleLinks.get(index)));
                     counter++;
                 }
             }
             index++;
+            if (index >= allArticleLinks.size()) {
+                break;
+            }
         }
 
+        int target = 3;
         counter = 0;
         int index2 = index;
-        while (counter != 4) {
+        while (counter != target) {
             if ((!allArticleLinks.get(index2).contains("_tps")) && allArticleLinks.get(index2).length() > 60) {
-                if (!selectedArticleLinks.contains(allArticleLinks.get(index2))) {
+                if (!topTenNBCArticles.containsKey(allArticleLinks.get(index2))) {
                     if (allArticleLinks.get(index2).contains("/us-news/")) {
-                        selectedArticleLinks.add(allArticleLinks.get(index2));
-                        // System.out.println(allArticleLinks.get(index));
+                        usNewsArticles.put(allArticleLinks.get(index2), getArticleText(allArticleLinks.get(index2)));
                         counter++;
                     }
                 }
             }
             index2++;
+            if (index2 >= allArticleLinks.size()) {
+                break;
+            }
         }
 
         counter = 0;
         index2 = index;
-        while (counter != 2) {
+        while (counter != target) {
             if ((!allArticleLinks.get(index2).contains("_tps")) && allArticleLinks.get(index2).length() > 60) {
-                if (!selectedArticleLinks.contains(allArticleLinks.get(index2))) {
+                if (!topTenNBCArticles.containsKey(allArticleLinks.get(index2))) {
                     if (allArticleLinks.get(index2).contains("/business/")) {
-                        selectedArticleLinks.add(allArticleLinks.get(index2));
-                        // System.out.println(allArticleLinks.get(index));
+                        businessArticles.put(allArticleLinks.get(index2), getArticleText(allArticleLinks.get(index2)));
                         counter++;
                     }
                 }
             }
             index2++;
+            if (index2 >= allArticleLinks.size()) {
+                break;
+            }
         }
 
         counter = 0;
         index2 = index;
-        while (counter != 4) {
+        while (counter != target) {
             if ((!allArticleLinks.get(index2).contains("_tps")) && allArticleLinks.get(index2).length() > 60) {
-                if (!selectedArticleLinks.contains(allArticleLinks.get(index2))) {
+                if (!topTenNBCArticles.containsKey(allArticleLinks.get(index2))) {
                     if (allArticleLinks.get(index2).contains("/world/")) {
-                        selectedArticleLinks.add(allArticleLinks.get(index2));
-                        // System.out.println(allArticleLinks.get(index));
+                        worldNewsArticles.put(allArticleLinks.get(index2), getArticleText(allArticleLinks.get(index2)));
                         counter++;
                     }
                 }
             }
             index2++;
+            if (index2 >= allArticleLinks.size()) {
+                break;
+            }
         }
 
         counter = 0;
         index2 = index;
-        while (counter != 4) {
+        while (counter != target) {
             if ((!allArticleLinks.get(index2).contains("_tps")) && allArticleLinks.get(index2).length() > 60) {
-                if (!selectedArticleLinks.contains(allArticleLinks.get(index2))) {
+                if (!topTenNBCArticles.containsKey(allArticleLinks.get(index2))) {
                     if (allArticleLinks.get(index2).contains("/politics/")) {
-                        selectedArticleLinks.add(allArticleLinks.get(index2));
-                        // System.out.println(allArticleLinks.get(index));
+                        politicsArticles.put(allArticleLinks.get(index2), getArticleText(allArticleLinks.get(index2)));
                         counter++;
                     }
                 }
             }
             index2++;
+            if (index2 >= allArticleLinks.size()) {
+                break;
+            }
         }
-
-
-
-        return selectedArticleLinks;
     }
 
-    public static void getArticleText(String url) throws IOException {
+    public static String getArticleText(String url) throws IOException {
 
         Document doc = Jsoup.connect(url).get();
 
-        String title = doc.title();
-
         Elements nameElements2 = doc.select("p");
 
-        // System.out.printf("Title: %s%n", title);
-
-        String placeholder = nameElements2.text().substring(161);
-        articleContents.add(placeholder);
-        articleHeadlines.add(title);
-
+        String text = nameElements2.text().substring(0, nameElements2.text().length() - 21);
+        return text;
     }
 
     public static void comparisons() {
 
-        String placeholder = "";
-
-        for (int i = 0; i < 10; i++) {
-            placeholder = placeholder + articleContents.get(i);
+        String articleText = "";
+        for (Entry<String, String> e : topTenNBCArticles.entrySet()) {
+            articleText += " " + e.getValue();
         }
+        Article top10NBC = new Article(articleText, "Top 10 NBC Articles");
 
-        Article top10FOX = new Article(placeholder, "Top 10 FOX Articles");
-
-        placeholder = "";
-
-        for (int i = 10; i < 14; i++) {
-            placeholder = placeholder + articleContents.get(i);
+        articleText = "";
+        for (Entry<String, String> e : usNewsArticles.entrySet()) {
+            articleText += " " + e.getValue();
         }
-        Article USFOX = new Article(placeholder, "FOX US Articles");
+        Article USNBC = new Article(articleText, "NBC US Articles");
 
-        placeholder = "";
-        for (int i = 14; i < 18; i++) {
-            placeholder = placeholder + articleContents.get(i);
+        articleText = "";
+        for (Entry<String, String> e : businessArticles.entrySet()) {
+            articleText += " " + e.getValue();
         }
-        Article entertainmentFOX = new Article(placeholder, "FOX Entertainment Articles");
+        Article businessNBC = new Article(articleText, "NBC Business Articles");
 
-        placeholder = "";
-        for (int i = 18; i < 22; i++) {
-            placeholder = placeholder + articleContents.get(i);
+        articleText = "";
+        for (Entry<String, String> e : worldNewsArticles.entrySet()) {
+            articleText += " " + e.getValue();
         }
-        Article worldFOX = new Article(placeholder, "FOX World Articles");
+        Article worldNBC = new Article(articleText, "NBC World Articles");
 
-        placeholder = "";
-        for (int i = 22; i < 26; i++) {
-            placeholder = placeholder + articleContents.get(i);
+        articleText = "";
+        for (Entry<String, String> e : politicsArticles.entrySet()) {
+            articleText += " " + e.getValue();
         }
-        Article politicsFOX = new Article(placeholder, "FOX Politics Articles");
+        Article politicsNBC = new Article(articleText, "NBC Politics Articles");
 
         ArrayList<Article> documents = new ArrayList<Article>();
-        documents.add(top10FOX);
-        documents.add(USFOX);
-        documents.add(entertainmentFOX);
-        documents.add(worldFOX);
-        documents.add(politicsFOX);
+        documents.add(top10NBC);
+        documents.add(USNBC);
+        documents.add(businessNBC);
+        documents.add(worldNBC);
+        documents.add(politicsNBC);
         Corpus corpus = new Corpus(documents);
         VectorSpaceModel vectorSpace = new VectorSpaceModel(corpus);
         for (int i = 0; i < documents.size(); i++) {
